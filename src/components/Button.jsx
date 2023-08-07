503fc8ea61981e0ea165fdd983fccb32edcc3c58
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-08-07 12:01:21 -0500

    Hero is done

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export const Button = ({styles, textButton}) => {
+    return (
+        <a className={styles}>
+            {textButton}
+        </a>
+    )
+}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,49 @@
 import hero from '../assets/hero.png';
+import { Button } from './Button';
 
 export const Hero = () => {
     return (
-        // <header>
-        //     <div
-        //         className="relative w-screen h-screen"
-        //         style={{ 
-        //             backgroundImage: `url(${hero})`,
-        //             filter: 'brightness(50%)',
-        //             height: '70vh'
-        //         }}
-        //     >
-        //         <div className="absolute">
-        //             <h1 className="text-white text-xl font-bold">Skyline</h1>
-        //         </div>
-        //     </div>
-        // </header>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <header className="bg-black h-[700px] md:h-[700px] text-center relative z-30">
+            <div className="z-40 relative container flex flex-col md:grid md:grid-cols-12 grid-rows-12 justify-center items-center h-full mx-auto md:pt-5 text-center">
+
+                <div className="col-start-2 col-end-12 md:col-start-5 md:col-end-9 grid grid-cols-12 grid-rows-12 justify-center items-center h-full">
+                    <div className="col-start-1 col-end-13 row-start-4 row-end-5">
+                        <h2 className="text-white text-2xl lg:text-3xl font-bold uppercase">Skyline</h2>
+                        <p className="text-white text-md mt-5">Transformando tu visión en una realidad digital</p>
+                    </div>
+
+                    <button className="col-start-5 col-end-9 row-start-6 row-end-7 mb-10 relative p-[9px] overflow-hidden text-black font-semibold text-md transition-colors duration-300 hover:text-gray-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 234 74" fill="none" className="absolute top-0 left-0 w-full h-full">
+                            <path d="M2 24.037L30.1469 2H232V54.5L205.462 72H2V24.037Z" stroke="#02B1BA" strokeWidth="3"/>
+                        </svg>
+                        <span className="relative z-10 text-white hover:text-gray-500">Ir a la página</span>
+                    </button>
+
+                </div>
+
+                <div className="md:col-start-12 md:col-end-13 flex md:flex-col items-center justify-center gap-10">
+                    <p className="md:rotate-90 text-cyan-400 uppercase text-[13px] font-bold">Social Networks</p>
+                    <div className="none md:border-l-2 border-cyan-400 h-24 mx-auto mt-10"></div>
+                    <div className="text-cyan-400 flex md:flex-col gap-3">
+                        <div>
+                            <i className="fa-brands fa-facebook-f hover:cursor-pointer"></i>
+                        </div>
+
+                        <div>
+                            <i className="fa-brands fa-twitter hover:cursor-pointer"></i>
+                        </div>
+
+                        <div>
+                            <i className="fa-brands fa-linkedin-in hover:cursor-pointer"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="bg-gradient-to-r from-gray-950 via-gray-950 to-gray-900 absolute top-0 bottom-0 h-full">
+                <img className="w-screen object-cover h-full object-center opacity-40" src={hero} alt="Hero Image" loading="lazy" />
+            </div>
+        </header>
     )
 }
