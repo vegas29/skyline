fbf129e7b46b71af6e6f62664c512cf3707d0456
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-08-07 20:51:23 -0500

    Hero fix and floating navbar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
-export const Button = ({styles, textButton}) => {
+export const Button = ({type, textButton}) => {
     return (
-        <a className={styles}>
-            {textButton}
-        </a>
+        <button className="col-start-5 col-end-9 row-start-6 row-end-7 mb-10 relative p-[9px] overflow-hidden text-black font-semibold text-md transition-colors duration-300 hover:text-gray-300">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 234 74" fill="none" className="absolute top-0 left-0 w-full h-full">
+                <path d="M2 24.037L30.1469 2H232V54.5L205.462 72H2V24.037Z" stroke="#02B1BA" strokeWidth="3"/>
+            </svg>
+            <span className="relative z-10 text-white hover:text-gray-300">{textButton}</span>
+        </button>
     )
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
+import BottomMenu from "../components/BottomMenu"
 import { Hero } from "../components/Hero"
 
 export const MainPage = () => {
   return (
     <>    
       <Hero/>
+      <BottomMenu/>
     </>
   )
 }
