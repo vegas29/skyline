19455e3e18251526b82f7ce64ddd094b2c3a26cb
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-08-07 20:52:39 -0500

    Fix navbar on mobile

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,6 +1,6 @@
 const BottomMenu = () => {
   return (
-    <nav className="fixed bottom-10 left-0 right-0 flex items-center gap-5 md:w-4/12 lg:w-3/12 mx-auto justify-center bg-black text-white p-5 shadow rounded z-40">
+    <nav className="fixed bottom-10 left-0 right-0 flex items-center gap-5 w-9/12 md:w-4/12 lg:w-3/12 mx-auto justify-center bg-black text-white p-5 shadow rounded z-40">
       <a href="#" className="uppercase hover:text-gray-300 hover:cursor-pointer">Skyline</a>
       <i className="fa-solid fa-user text-lg hover:text-gray-300 hover:cursor-pointer"></i>
       <i className="fa-solid fa-laptop text-lg hover:text-gray-300 hover:cursor-pointer"></i>
