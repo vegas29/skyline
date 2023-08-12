637fbd2d5c01f69700e96edfde3e3918668763bc
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-08-12 18:02:20 -0500

    Fix some styles on title in services

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-
 export const ServiceCard = ({ imageSrc, defaultText, hoverText }) => {
-
   return (
     <div
       className={`relative overflow-hidden w-full h-full md:m-4 hover:cursor-pointer transition-transform hover:-translate-y-5`}
       style={{ boxShadow: '14px 14px 1px 0px rgba(2, 177, 186, 1)'}}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
     >
       <img
         src={imageSrc}
@@ -18,7 +13,7 @@ export const ServiceCard = ({ imageSrc, defaultText, hoverText }) => {
         className={`absolute inset-0 flex items-center justify-center`}
       >
         <div className="bg-black bg-opacity-50 w-full h-full flex items-center justify-center">
-          <p className="text-white text-center">{defaultText}</p>
+          <p className="text-white text-center md:text-xl">{defaultText}</p>
         </div>
       </div>
     </div>
